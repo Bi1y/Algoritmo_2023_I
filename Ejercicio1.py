816647a0d48c5e749b9910863bdb63c576dbaac0
Bi1y <billy.gago@unas.edu.pe>
2023-08-08 19:11:20 -0500

    se agrega

---
+++ Ejercicio1.py
@@ -0,0 +1,16 @@
+# Escribir un Pseudocódigo de un programa que permita leer la edad y peso.
+# Variables edad, peso.
+edad = 0
+peso = 0
+
+# Imprimir "Escribir los datos (Edad, Peso):"
+print("Escribir los datos (Edad, Peso):")
+
+# Leer Edad, Leer Peso.
+edad = int(input("Ingrese la edad: "))
+peso = float(input("Ingrese el peso: "))
+
+# Visualizar "Tu peso es: ", peso, " y tu edad es: ", edad.
+print("Tu peso es:", peso, "y tu edad es:", edad)
+
+# Fin
